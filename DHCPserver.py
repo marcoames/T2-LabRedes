1f0d9c96e204f8f74de4c836f4988e5230e9d0e8
marco <marco.ames@edu.pucrs.br>
2024-06-29 01:43:18 -0300

    updated packet slicing and checksum calculation

--- DHCPserver.py
+++ DHCPserver.py
@@ -12,14 +12,17 @@ def get_mac_address():
 
 # Checksum dos pacotes
 def calculate_checksum(msg):
+    if len(msg) % 2 == 1:
+        msg += b'\x00'
+    
     s = 0
     for i in range(0, len(msg), 2):
         w = (msg[i] << 8) + (msg[i+1])
-        s = s + w
+        s += w
 
     s = (s >> 16) + (s & 0xffff)
+    s += (s >> 16)
     s = ~s & 0xffff
-
     return s
 
 # Criação do socket RAW
@@ -107,12 +110,11 @@ def dhcp_offer(transaction_id, server_mac, client_mac, server_ip, offered_ip, ma
 
     # Calcula UDP length e checksum
     udp_length = 8 + len(dhcp_header) + len(options)
-    header = source_ip + dest_ip + b'\x00' + b'\x11' + udp_length.to_bytes(2, byteorder='big')
 
     # UDP header com length
     udp_header = source_port + dest_port + udp_length.to_bytes(2, byteorder='big') + checksum
 
-    udp_checksum = calculate_checksum(header + udp_header + dhcp_header + options)
+    udp_checksum = calculate_checksum(udp_header + dhcp_header + options)
     udp_checksum = udp_checksum.to_bytes(2, byteorder='big')
 
     # UDP header com checksum
@@ -120,6 +122,7 @@ def dhcp_offer(transaction_id, server_mac, client_mac, server_ip, offered_ip, ma
 
     # Pacote final
     packet = eth_header + ip_header + udp_header + dhcp_header + options
+    packet = ip_header + udp_header + dhcp_header + options
 
     print("Enviando Pacote Offer: ", packet)
 
@@ -175,7 +178,6 @@ def dhcp_ack(transaction_id, server_mac, client_mac, server_ip, offered_ip, masc
     secs = b'\x00\x00'
     flags = b'\x00\x00'       
     cli_addr = b'\x00\x00\x00\x00'
-    #your_cli_addr = socket.inet_aton(offered_ip)
     your_cli_addr = offered_ip
     next_server_ip_addr = b'\x00\x00\x00\x00'
     relay_agent_ip_addr = b'\x00\x00\x00\x00'
@@ -201,12 +203,11 @@ def dhcp_ack(transaction_id, server_mac, client_mac, server_ip, offered_ip, masc
 
     # Calcula UDP length e checksum
     udp_length = 8 + len(dhcp_header) + len(options)
-    header = source_ip + dest_ip + b'\x00' + b'\x11' + udp_length.to_bytes(2, byteorder='big')
 
     # UDP header com length
     udp_header = source_port + dest_port + udp_length.to_bytes(2, byteorder='big') + checksum
 
-    udp_checksum = calculate_checksum(header + udp_header + dhcp_header + options)
+    udp_checksum = calculate_checksum(udp_header + dhcp_header + options)
     udp_checksum = udp_checksum.to_bytes(2, byteorder='big')
 
     # UDP header com checksum
@@ -220,6 +221,74 @@ def dhcp_ack(transaction_id, server_mac, client_mac, server_ip, offered_ip, masc
     return packet
 
 
+def dhcp_offer2(transaction_id, ip_header, udp_header, dhcp_header, dhcp_options, server_ip, server_mac, client_mac, dns_ip):
+    print("\n")
+    print("IP header: ", ip_header.hex())
+    print("UDP header: ", udp_header.hex())
+    print("DHCP header: ", dhcp_header.hex())
+    print("DHCP options: ", dhcp_options.hex())
+
+    # ETH HEADER
+    new_eth_header = bytearray(14)
+    new_eth_header[:6] = client_mac
+    new_eth_header[6:12] = server_mac
+    new_eth_header[12:14] = b'\x08\x00'
+
+    print("NEW_ETH header: ", new_eth_header.hex())
+
+    # IP HEADER
+    new_ip_header = bytearray(ip_header)
+    new_ip_header[:16] = ip_header[:16]
+    new_ip_header[12:16] = socket.inet_aton(server_ip)
+    new_ip_header[17:20] = client_mac
+
+    ip_checksum = calculate_checksum(ip_header)
+    new_ip_header[10:12] = ip_checksum.to_bytes(2, byteorder='big')
+
+    print("NEW_IP header: ", new_ip_header.hex())
+
+    # UDP HEADER
+    new_udp_header = bytearray(udp_header)
+    new_udp_header[0:2] = b'\x00\x44'   # Source Port
+    new_udp_header[2:4] = b'\x00\x43'   # Dest Port
+    new_udp_header[4:] = udp_header[4:]
+
+    print("NEW_UDP header: ", new_udp_header.hex())
+
+    # DHCP HEADER
+    new_dhcp_header = bytearray(dhcp_header)
+    new_dhcp_header[0:1] = b'\x02'
+    new_dhcp_header[1:17] = dhcp_header[1:17]
+    new_dhcp_header[17:21] = socket.inet_aton(server_ip)
+    new_dhcp_header[21:28] = dhcp_header[21:28]
+    new_dhcp_header[28:33] = client_mac
+    new_dhcp_header[33:254] = dhcp_header[33:254]
+
+    print("NEW_DHCP header: ", new_dhcp_header.hex())
+
+    # DHCP Options
+    new_dhcp_options = bytearray(dhcp_options)
+    new_dhcp_options[:3] =  b'\x35\x01\x05'
+    new_dhcp_options[3:9] = socket.inet_aton(server_ip)
+    new_dhcp_options[9:15] = b'\x33\x04\x00\x00\x0e\x10'  # Lease time 3600s
+    new_dhcp_options[15:21] = b'\x01\x04\xff\xff\xff\x00'  # Mascara
+    new_dhcp_options[21:26] = b'\x03\x04' + socket.inet_aton(server_ip)
+    new_dhcp_options[26:36]= b'\x06\x08' + socket.inet_aton(dns_ip) +  socket.inet_aton(dns_ip) # DNS server
+    new_dhcp_options[36:42] = b'\x0f\x04\x00\x00\x00\x00' # Domain Name
+    new_dhcp_options[36:37] = b'\xff'
+    new_dhcp_options[37:]= b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' # Padding
+
+    print("NEW_DHCP options: ", new_dhcp_options.hex())
+
+    packet = new_eth_header + new_ip_header + new_udp_header + new_dhcp_header + new_dhcp_options
+    packet = new_ip_header + new_udp_header + new_dhcp_header + new_dhcp_options
+
+    print("\n")
+    print("PACKET: ", packet)
+    print("\n")
+
+    return packet
+
 # Função principal para executar o servidor DHCP
 def dhcp_server():
     hostname = socket.gethostname()
@@ -227,7 +296,7 @@ def dhcp_server():
     s = create_socket()
     print("SERVER IP: ", server_ip)
     server_mac = get_mac_address()
-    print("Server MAC: ", server_mac)
+    print("Server MAC: ", server_mac.hex())
 
     print("Servidor em execução...")
 
@@ -239,14 +308,13 @@ def dhcp_server():
 
     while True:
         try:
-            print("Escutando...")
+            print("\nEscutando...")
             packet, addr = s.recvfrom(4096)
 
             print("Pacote Recebido:", packet)
             print("Endereço:", addr)
 
-            message_type = packet[284-14]
-            print("Message Type: ", message_type)
+   
 
             # dados do header eth
             # eth_header = packet[0:14]
@@ -254,23 +322,35 @@ def dhcp_server():
 
             # dados do header ip
             ip_header = packet [0:20]
-            print("IP header: ", ip_header.hex())
+            #print("IP header: ", ip_header.hex())
 
-            # transaction_id = dhcp_header[4:8]
-            transaction_id = packet[46-14:50-14]
-            print("Transaction ID: ", transaction_id.hex())
-            client_mac = packet[70-14:76-14]
+            # dados do header udp
+            udp_header = packet[20:28]
+
+            # dados do header dhcp
+            dhcp_header = packet[28:268]
+
+            # dados dhcp options
+            dhcp_options = packet[268:]
+
+            transaction_id = packet[32:36]
+            print("\nTransaction ID: ", transaction_id.hex())
+            client_mac = packet[56:61]
             print("Client MAC: ", client_mac.hex())
             #client_ip = ??
 
+            message_type = packet[270]
+            print("Message Type: ", message_type)
+
             # Envia Pacotes de Offer e Ack
             if message_type == 1:
-                print(f'DHCP Discover recebido de {addr}, enviando DHCP Offer')
+                print(f'\nDHCP Discover recebido de {addr}, enviando DHCP Offer')
                 packet = dhcp_offer(transaction_id, server_mac, client_mac, server_ip, offered_ip, mascara, router_ip, dns_ip)
+                #packet = dhcp_offer2(transaction_id, ip_header, udp_header, dhcp_header, dhcp_options, server_ip, server_mac, client_mac, dns_ip)
                 s.sendto(packet, ('<broadcast>', 68))
 
             elif message_type == 3:
-                print(f'DHCP Request recebido de {addr}, enviando DHCP Acknowledgement')
+                print(f'\nDHCP Request recebido de {addr}, enviando DHCP Acknowledgement')
                 packet = dhcp_ack(transaction_id, server_mac, client_mac, server_ip, offered_ip, mascara, router_ip, dns_ip)
                 s.sendto(packet, ('<broadcast>', 68))
         
